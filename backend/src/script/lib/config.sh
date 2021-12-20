b289f5c50f39b8a8bb9a72ddb868af2644cf5e76
Martin Aschoff <ma@agnitas.de>
2021-12-20 16:43:31 +0100

    backend update to 21.10.000.108

--- backend/src/script/lib/config.sh
+++ backend/src/script/lib/config.sh
@@ -38,7 +38,7 @@ export	DBCFG_PATH="$BASE/etc/dbcfg"
 #
 verbose=1
 quiet=0
-version="21.10.000.064"
+version="21.10.000.108"
 licence="`$BASE/bin/config-query licence`"
 system="`uname -s`"
 host="`uname -n | cut -d. -f1`"
